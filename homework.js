c486bae4fe4513f488498d12b57e091fb0b07e0c
KotlovDmitry <62717446+KotlovDmitry@users.noreply.github.com>
2020-12-06 23:29:33 +0300

    Add files via upload

---
+++ homework.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Часть 1
+let Celsius = +prompt("Введите температуру по Цельсию");
+let Fahrenheit = (9 / 5) * Celsius + 32;
+alert(Fahrenheit);
+
+// Часть 2(3)
+let name = "Василий";
+let admin = name;
+alert(admin);
+
+// Часть 4
+// 1000108
