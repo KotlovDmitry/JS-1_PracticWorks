cd11a9de2ecebf227de101cbd762cc838eb961bf
KotlovDmitry <kotlov.dmitry@gmail.com>
2020-12-18 19:40:44 +0300

    lesson_3

--- step_3.js
+++ step_3.js
@@ -1,6 +1,7 @@
 'use strict';
+// Возьмем для массива данные из второй задачи
 
-let cart = [
+let cart_2 = [
     {
         product: "Японский язык для начинающих Ч.1. Нечаева",
         price: 1214
@@ -14,9 +15,11 @@ let cart = [
         price: 1174
     }
 ];
-let basketPrice = 0;
-for (let prod of cart){
-    basketPrice += prod.price;
-    console.log("\"" + prod.product + "\" стоит: " + prod.price + " р.");
+function countBasketPrice(cart_2) {
+    let funBasketPrice = 0;
+    for (let prod of cart_2){
+        funBasketPrice += prod.price;
+    }
+    return funBasketPrice;
 }
-console.log("Итоговая стоимость: " + basketPrice + " р.");
+console.log("Стоимость корзины: " + countBasketPrice(cart_2) + " р.");
---
+++ step_5.js
@@ -0,0 +1,5 @@
+let letters = 'x';
+        for (let i = 0; i < 20; i++){
+            console.log(letters);
+            letters += 'x';
+        }
