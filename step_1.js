21c12a0115c45f177d6b655c7fd9fc638ece774a
KotlovDmitry <kotlov.dmitry@gmail.com>
2020-12-21 22:56:48 +0300

    Немного переделано задание 1

--- step_1.js
+++ step_1.js
@@ -8,16 +8,20 @@ let digit = {
 };
 if (digit.number <= 9){
     digit.units = digit.number;
+    console.log('Единиц: ', digit.number);
 } else if (digit.number > 9 && digit.number <= 99){
     digit.units = Math.floor(digit.number % 10);
     digit.tens = Math.floor(digit.number / 10 % 10);
-    digit.hundreds = Math.floor(digit.number / 100 % 10);
-}else if (digit.number > 99 && digit.number <= 999) {
+    console.log('Единиц: ', digit.units);
+    console.log('Десятков: ', digit.tens);
+} else if (digit.number > 99 && digit.number <= 999) {
     digit.units = Math.floor(digit.number % 10);
     digit.tens = Math.floor(digit.number / 10 % 10);
     digit.hundreds = Math.floor(digit.number / 100 % 10);
-}else {
+    console.log('Единиц: ', digit.units);
+    console.log('Десятков: ', digit.tens);
+    console.log('Сотен: ', digit.hundreds);
+} else {
     digit.number = 0;
     console.log('Вы ввели число, не входящее в указанный диапазон');
-}
-console.log(digit);
+};
